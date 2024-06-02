38a9ae99a021527dce4f7963b9f14fe5230863d6
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2024-06-02 18:28:05 +0200

    refix config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 // Database connection
 
-define('DB_HOST', 'us-cluster-east-01.k8s.cleardb.net');
-define('DB_USERNAME', 'bb269a50fef1e0');
-define('DB_PASSWORD', '0c9c1661');
-define('DB_NAME', 'heroku_f2062b3d1a7203f');
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'restaurant');
 define('DB_PORT', '');
 
 // JWT Token
