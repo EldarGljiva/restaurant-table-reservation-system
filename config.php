32c21c35ea8dc8ec6fd09847be02cb7e31db6932
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2024-06-02 18:16:17 +0200

    update config.php

--- config.php
+++ config.php
@@ -1,14 +1,12 @@
 <?php
 
-// Parse the CLEARDB_DATABASE_URL provided by Heroku
-$cleardb_url = parse_url(getenv('CLEARDB_DATABASE_URL'));
-
 // Database connection
-define('DB_HOST', $cleardb_url['host']);
-define('DB_USERNAME', $cleardb_url['user']);
-define('DB_PASSWORD', $cleardb_url['pass']);
-define('DB_NAME', substr($cleardb_url['path'], 1)); // Remove the leading '/' from the path
-define('DB_PORT', $cleardb_url['port']);
+
+define('DB_HOST', 'us-cluster-east-01.k8s.cleardb.net');
+define('DB_USERNAME', 'bb269a50fef1e0');
+define('DB_PASSWORD', '0c9c1661');
+define('DB_NAME', 'heroku_f2062b3d1a7203f');
+define('DB_PORT', '');
 
 // JWT Token
-define('JWT_SECRET', getenv('JWT_SECRET'));
+define('JWT_SECRET', 'web');
